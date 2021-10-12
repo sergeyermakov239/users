1629b5e715f01fd605b0c91dedfbba0159440ba4
sergeyermakov239 <sergeyermakov13112004@gmail.com>
2021-10-12 13:18:36 +0300

    initial commit

---
+++ src/main/java/sergey/ermakov/project1/ApiController.java
@@ -0,0 +1,17 @@
+package sergey.ermakov.project1;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+public class ApiController {
+
+    @GetMapping("books")
+    public ResponseEntity<String> getText() {
+        return ResponseEntity.ok("Hello text");
+    }
+
+}
